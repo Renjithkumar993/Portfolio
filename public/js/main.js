01fded231be6736deeaca250f4bf181938ac874c
Renjithkumar993 <121647888+Renjithkumar993@users.noreply.github.com>
2023-05-17 09:52:38 -0300

    added more images

--- public/js/main.js
+++ public/js/main.js
@@ -8,3 +8,5 @@ setTimeout(function () {
 
 
 
+
+
--- server.js
+++ server.js
@@ -23,3 +23,4 @@ app.use(require('./controllers/portfolioroute'));
 app.listen(PORT, () => {
   console.log('Server listening on: http://localhost:' + PORT);
 });
+
